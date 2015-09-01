9f5db352ab8978b419ddf1c5b901b6bbb8bb81e0
Rustam Ibragimov <exploit2002@gmail.com>
2015-09-01 18:49:04 +1100

    New scaffold: Microposts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150901054735) do
+ActiveRecord::Schema.define(version: 20150901074526) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.string   "ontent"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
